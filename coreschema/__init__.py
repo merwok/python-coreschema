84ee6a3dfea76e48eb011ff4f3258f60f77f22f5
Éric Araujo <earaujo@caravan.coop>
2018-12-20 18:35:16 -0500

    fix __all__

--- coreschema/__init__.py
+++ coreschema/__init__.py
@@ -1,7 +1,7 @@
 from coreschema.schemas import (
     Object, Array, Integer, Number, String, Boolean, Null,
     Enum, Anything, Ref, RefSpace,
-    Union, Intersection, ExclusiveUnion, Not
+    Union, Intersection, ExclusiveUnion, Not,
 )
 from coreschema.encodings.html import render_to_form
 
@@ -9,8 +9,8 @@ from coreschema.encodings.html import render_to_form
 __version__ = '0.0.4'
 
 __all__ = [
-    Object, Array, Integer, Number, String, Boolean, Null,
-    Enum, Anything, Ref, RefSpace,
-    Union, Intersection, ExclusiveUnion, Not,
-    render_to_form
+    'Object', 'Array', 'Integer', 'Number', 'String', 'Boolean', 'Null',
+    'Enum', 'Anything', 'Ref', 'RefSpace',
+    'Union', 'Intersection', 'ExclusiveUnion', 'Not',
+    'render_to_form',
 ]
